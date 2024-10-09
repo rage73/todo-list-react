bddef827f4988dc9003b6a3bf6d1426076b5a5e2
rage73 <raghavagrawal2000ra@gmail.com>
2024-10-10 02:24:27 +0530

    search debounce

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,20 +1,28 @@
 import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { TodoListContext } from "../contexts/todoListContext";
 
 const SearchBar: React.FC = () => {
-    const { searchText, setSearchText } = useContext(TodoListContext);
+    const { setSearchText } = useContext(TodoListContext);
+    const [text, setText] = useState<string>("");
+
+    useEffect(() => {
+        const debounceSearch = setTimeout(() => {
+            setSearchText(text);
+        }, 300);
+        return () => clearTimeout(debounceSearch);
+    }, [text]);
 
     return (
         <div className="w-full relative">
             <input
                 className="w-full border border-gray-500 rounded-[32px] flex items-center p-2 px-10"
                 placeholder="Search"
-                value={searchText}
-                onChange={e => setSearchText(e.target.value)}
+                value={text}
+                onChange={e => setText(e.target.value)}
             />
             <MagnifyingGlassIcon className="size-6 absolute left-2 top-2" />
-            {searchText && <XMarkIcon className="cursor-pointer size-6 absolute right-2 top-2" onClick={() => setSearchText("")} />}
+            {text && <XMarkIcon className="cursor-pointer size-6 absolute right-2 top-2" onClick={() => setText("")} />}
         </div>
     );
 }
