1cf8178de11351a8879a19bb7a0a658b391885a7
rage73 <raghavagrawal2000ra@gmail.com>
2024-10-09 20:22:48 +0530

    searchbar comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import SearchBar from "./components/searchBar"
+import { TodoListContext } from "./contexts/todoListContext";
 
 function App() {
+  const [searchText, setSearchText] = useState<string>("");
 
   return (
     <div className="w-full flex justify-center bg-white">
-      <div className="mt-10 w-4/5 flex-col items-center">
-        <div className="flex justify-between items-center gap-10">
-          <div>Today</div>
-          <SearchBar />
-          <div></div>
+      <TodoListContext.Provider value={{ searchText, setSearchText }}>
+        <div className="mt-10 w-4/5 flex-col items-center">
+          <div className="flex justify-between items-center gap-10">
+            <div>Today</div>
+            <SearchBar />
+            <div></div>
+          </div>
+          <div>{searchText}</div>
         </div>
-        <div></div>
-      </div>
-
+      </TodoListContext.Provider>
     </div>
   )
 }
--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,17 +1,22 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import React from "react";
+import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useContext } from "react";
+import { TodoListContext } from "../contexts/todoListContext";
 
-const SearchBar: React.FC<SearchBarProps> = (props) => {
+const SearchBar: React.FC = () => {
+    const { searchText, setSearchText } = useContext(TodoListContext);
+
     return (
         <div className="w-full relative">
-            <input className="w-full border border-black rounded-2xl flex items-center p-2 px-8" placeholder="Search"/>
+            <input
+                className="w-full border border-gray-500 rounded-[32px] flex items-center p-2 px-10"
+                placeholder="Search"
+                value={searchText}
+                onChange={e => setSearchText(e.target.value)}
+            />
             <MagnifyingGlassIcon className="size-6 absolute left-2 top-2" />
+            {searchText && <XMarkIcon className="cursor-pointer size-6 absolute right-2 top-2" onClick={() => setSearchText("")} />}
         </div>
     );
 }
 
 export default SearchBar;
-
-export interface SearchBarProps {
-
-}
